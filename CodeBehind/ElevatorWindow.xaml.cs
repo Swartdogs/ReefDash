aeb9ef93ef31488afe6fcd4bcc41bd38a7823465
Collin McIntyre <crm11396@gmail.com>
2025-01-22 13:52:08 -0600

    add elevator window

---
+++ CodeBehind/ElevatorWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+
+namespace ReefDash;
+
+/// <summary>
+/// Interaction logic for ElevatorWindow.xaml
+/// </summary>
+public partial class ElevatorWindow : Window
+{
+    private readonly MainWindow _mainWindow;
+
+    public ElevatorWindow(MainWindow mainWindow)
+    {
+        InitializeComponent();
+
+        _mainWindow = mainWindow;
+    }
+
+    private void ZeroMax(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ZeroL4(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ZeroL3(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ZeroL2(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ZeroL1(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ZeroStow(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ZeroMin(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
--- CodeBehind/MainWindow.xaml.cs
+++ CodeBehind/MainWindow.xaml.cs
@@ -7,8 +7,28 @@ namespace ReefDash;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private ElevatorWindow? _elevatorWindow = null;
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ToggleElevatorWindow(object sender, RoutedEventArgs e)
+    {
+        if (_elevatorWindow is null)
+        {
+            _elevatorWindow = new ElevatorWindow(this);
+            _elevatorWindow.Closed += (s, e) =>
+            {
+                _elevatorWindow = null;
+            };
+            _elevatorWindow.Show();
+        }
+        else
+        {
+            _elevatorWindow.Close();
+            _elevatorWindow = null;
+        }
     }
 }
