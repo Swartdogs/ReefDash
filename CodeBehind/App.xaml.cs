64afdebbdeeeac02519d67ed11c1ea7b4c9505dc
Collin McIntyre <crm11396@gmail.com>
2025-01-22 12:21:44 -0600

    organize source code

---
+++ CodeBehind/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace ReefDash;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    [STAThread]
+    public static void Main()
+    {
+        App app = new App();
+        app.Run(new MainWindow());
+    }
+}
